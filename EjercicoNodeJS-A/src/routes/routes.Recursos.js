f31b81b6532451277004c5af9a4ec14459e13572
Sharit-Vargas07 <sharitvargas07@gmail.com>
2024-03-08 08:29:17 -0500

    nuevo

--- EjercicoNodeJS-A/src/routes/routes.Recursos.js
+++ EjercicoNodeJS-A/src/routes/routes.Recursos.js
@@ -1,15 +1,12 @@
 import { Router } from "express";
 import {registrar,  actualizar,  buscar, listar } from '../controller/controllerRecursos.js';
-import { validacionRecursosR } from "../../validate/validacionRecursos.js";
-import { validacionRecursosA } from "../../validate/validacionRecursos.js";
-
 
 const rutaRecursos = Router();
 
 rutaRecursos.get('/listar', listar);
-rutaRecursos.post('/registrar',validacionRecursosR, registrar);
-rutaRecursos.get('/buscar/:id_recursos',  buscar); 
-rutaRecursos.put('/actualizar/:id_recursos',validacionRecursosA, actualizar);
+rutaRecursos.post('/registrar', registrar);
+rutaRecursos.get('/buscar/:id_recursos', buscar); 
+rutaRecursos.put('/actualizar/:id_recursos', actualizar);
 
 
 export default rutaRecursos;
